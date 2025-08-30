635b3fea0d3126346e7432d9af41b050315d4bcf
Deepal Tembhurne <142749933+Deepal22@users.noreply.github.com>
2025-08-31 04:11:23 +0530

    Time: 3 ms (62.69%) | Memory: 22 MB (77.84%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        vector<unordered_set<char>> rows(9), cols(9), blocks(9);
-
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                char c = board[i][j];
-                if (c == '.') continue;
-
-                int boxIdx = (i / 3) * 3 + (j / 3);
-
-                if (rows[i].count(c) || cols[j].count(c) || blocks[boxIdx].count(c)) {
-                    return false;
+        for(int i=0;i<9;i++) {
+            vector<bool> isPresentRow(9,false);
+            vector<bool> isPresentColumn(9,false);
+            for(int j=0;j<9;j++) {
+                if(board[i][j] != '.') {
+                    if(isPresentRow[board[i][j] - '1']) return false;
+                    isPresentRow[board[i][j] - '1'] = true;
                 }
+                if(board[j][i] != '.') {
+                    if(isPresentColumn[board[j][i] - '1']) return false;
+                    isPresentColumn[board[j][i] - '1'] = true;
+                }
+            }
+        }
 
-                rows[i].insert(c);
-                cols[j].insert(c);
-                blocks[boxIdx].insert(c);
+        for(int i=0;i<9;i++) {
+            vector<bool> isPresentBox(9,false);
+            int rowShift = (i/3)*3;
+            int columnShift = (i%3)*3;
+            for(int j=0;j<3;j++) {
+                for(int k=0;k<3;k++) {
+                    if(board[rowShift+j][columnShift+k] != '.') {
+                        int num = board[rowShift+j][columnShift+k] - '1';
+                        if(isPresentBox[num]) return false;
+                        isPresentBox[num] = true;
+                    }
+                }
             }
         }
         return true;
