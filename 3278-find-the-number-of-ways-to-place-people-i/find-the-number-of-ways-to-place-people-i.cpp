05520b957d18c5db4c78eec013d5869c77f1f3c8
Deepal Tembhurne <142749933+Deepal22@users.noreply.github.com>
2025-09-03 00:02:40 +0530

    Time: 0 ms (100.00%) | Memory: 32.5 MB (74.81%) - LeetSync

--- 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     int numberOfPairs(vector<vector<int>>& points) {
-        sort(points.begin(), points.end(), [](const auto& a, const auto& b) {
-            if (a[0] == b[0]) return a[1] > b[1]; // y descending if x equal
-            return a[0] < b[0];                   // x ascending
-        });
+        sort(points.begin(), points.end(), [](const auto &a, const auto &b) { return a[0] == b[0] ? a[1] > b[1] : a[0] < b[0]; });
 
-        int n = points.size(), result = 0;
-        for (int i = 0; i < n; i++) {
-            int top = points[i][1];
-            int bot = INT_MIN;
-            for (int j = i + 1; j < n; j++) {
-                int y = points[j][1];
-                if (bot < y && y <= top) {
-                    result++;
-                    bot = y;
-                    if (bot == top) break;
+        int res = 0, n = points.size();
+        for (int i = 0; i < n; i++)
+        {
+            int b = points[i][1], last = -1;
+            for (int j = i + 1; j < n; j++)
+            {
+                int a = points[j][1];
+                if (a <= b && a > last)
+                {
+                    last = a;
+                    ++res;
                 }
             }
         }
-        return result;
+
+        return res;
     }
 };
