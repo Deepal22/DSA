b17b49891775ac927d3549e4068df629ec78e5f9
Deepal Tembhurne <142749933+Deepal22@users.noreply.github.com>
2025-09-06 01:13:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (11.02%) - LeetSync

--- 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.cpp
+++ 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int makeTheIntegerZero(int num1, int num2) {
-        for (int k = 1; k <= 60; k++) {
-            long long x = num1 - 1LL * num2 * k;
+        int k = 1;
+        while (true) {
+            long long x = num1 - static_cast<long long>(num2) * k;
             if (x < k) {
                 return -1;
             }
             if (k >= __builtin_popcountll(x)) {
                 return k;
             }
+            k++;
         }
-        return -1;
     }
 };
