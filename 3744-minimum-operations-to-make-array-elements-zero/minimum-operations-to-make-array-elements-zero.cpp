f161830682ec1547f776e00214e91b6051440874
Deepal Tembhurne <142749933+Deepal22@users.noreply.github.com>
2025-09-06 17:32:31 +0530

    Time: 8 ms (90.58%) | Memory: 183.4 MB (50.00%) - LeetSync

--- 3744-minimum-operations-to-make-array-elements-zero/minimum-operations-to-make-array-elements-zero.cpp
+++ 3744-minimum-operations-to-make-array-elements-zero/minimum-operations-to-make-array-elements-zero.cpp
@@ -1,28 +1,24 @@
 class Solution {
+private:
+    int flog4(int n){
+        return (31-__builtin_clz(n)) >> 1;
+    }
+    long long pfx(int n){
+        int p = flog4(n);
+        long long pow4p = 1 << p*2;
+        long long N = (3*p-1) * pow4p + 1;
+        N /= 3;
+        N += (p+1) * (n-pow4p+1);
+        return N;
+    }
 public:
     long long minOperations(vector<vector<int>>& queries) {
-        long long ans = 0;
-        for (auto &q : queries) {
-            int l = q[0], r = q[1];
-            long long S = 0;
-            int dMax = 0;
-
-            for (int k = 1; k <= 31; k++) {
-                long long low = 1LL << (k - 1);
-                long long high = (1LL << k) - 1;
-                if (low > r) break;
-                long long a = max((long long)l, low);
-                long long b = min((long long)r, high);
-                if (a > b) continue;
-                long long cnt = b - a + 1;
-                int stepsForK = (k + 1) / 2;
-                S += cnt * stepsForK;
-                dMax = max(dMax, stepsForK);
-            }
-
-            long long ops = max((long long)dMax, (S + 1) / 2);
-            ans += ops;
+        long long res = 0;
+        for(auto &q: queries){
+            long long N = pfx(q[1]);
+            if(q[0]>1) N -= pfx(q[0]-1);
+            res += (N>>1) + (N&1);
         }
-        return ans;
+        return res;
     }
 };
