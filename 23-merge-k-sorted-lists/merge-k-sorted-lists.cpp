83a5a016052daf8bfff7e45a3aa838dfab76cf58
Deepal Tembhurne <142749933+Deepal22@users.noreply.github.com>
2025-09-07 15:08:21 +0530

    Time: 2 ms (70.38%) | Memory: 18.9 MB (30.43%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -10,39 +10,34 @@
  */
 class Solution {
 public:
-    ListNode* mergeKLists(vector<ListNode*>& arr) {
-        if(arr.size() == 0) return nullptr ;
-        if(arr.size() < 2) return arr[0] ;
-        ListNode * dummy = new ListNode(0) ;
-        ListNode * temp = dummy ;
-        ListNode * node1  = arr[0] ;
-        ListNode * node2 = arr[1] ;
-        for(int i = 1 ; i < arr.size() ; i++){
-            temp = dummy;
-            if(i == 1)
-            node1 = arr[i-1];
-            else{
-                node1 = dummy->next;
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+        
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                tail->next = l1;
+                l1 = l1->next;
+            } else {
+                tail->next = l2;
+                l2 = l2->next;
             }
-            node2 = arr[i];
-        while(node1 && node2){
-            if(node1->val < node2->val){
-                temp->next = node1;
-                node1 = node1->next;
-                temp = temp->next;
-            }else{
-                temp->next = node2;
-                node2 = node2->next;
-                temp = temp->next;
+            tail = tail->next;
+        }
+        tail->next = l1 ? l1 : l2;
+        return dummy.next;
+    }
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if (lists.empty()) return nullptr;
+        int interval = 1;
+        int n = lists.size();
+        while (interval < n) {
+            for (int i = 0; i + interval < n; i += interval * 2) {
+                lists[i] = mergeTwoLists(lists[i], lists[i + interval]);
             }
+            interval *= 2;
         }
-        if(node1){
-            temp->next = node1;
-        }
-        if(node2){
-            temp->next= node2;
-        }
-        }
-        return dummy->next;
+        return lists[0];
     }
 };
