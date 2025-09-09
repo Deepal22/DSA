2d604253970e1db43984d76127663a2322e2c9a0
Deepal Tembhurne <142749933+Deepal22@users.noreply.github.com>
2025-09-09 18:08:16 +0530

    Time: 5 ms (54.74%) | Memory: 18.2 MB (11.68%) - LeetSync

--- 2408-number-of-people-aware-of-a-secret/number-of-people-aware-of-a-secret.py
+++ 2408-number-of-people-aware-of-a-secret/number-of-people-aware-of-a-secret.py
@@ -1,19 +1,12 @@
 class Solution:
     def peopleAwareOfSecret(self, n: int, delay: int, forget: int) -> int:
-        MOD = 10**9 + 7
-        if n == 1:
-            return 1
-        dp = [0] * (n + 1)
-        dp[1] = 1
-        window = 0
-        for i in range(2, n + 1):
-            enter = i - delay
-            exit_ = i - forget
-            if enter >= 1:
-                window = (window + dp[enter]) % MOD
-            if exit_ >= 1:
-                window = (window - dp[exit_] + MOD) % MOD
-            dp[i] = window
-        start = max(1, n - forget + 1)
-        ans = sum(dp[start: n+1]) % MOD
-        return ans
+        dp = [0]*n
+        dp[0] = 1
+        s = 0
+        for i in range(delay,n):
+            s += dp[i-delay]
+            dp[i] = s
+            if i-forget+1 >= 0:
+                s -= dp[i-forget+1]
+        #print(dp[-forget:])
+        return(sum(dp[-forget:]))%(10**9+7)
