f6622357272810e45be2183f323428da53cf135d
Deepal Tembhurne <142749933+Deepal22@users.noreply.github.com>
2025-09-12 23:57:57 +0530

    Time: 3 ms (87.13%) | Memory: 16.6 MB (99.44%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
-    bool doesAliceWin(string s) {
-        for (int i = 0; i < s.size(); i++)
-            if ((0x104111 >> (s[i] - 97)) & 1)
-                return 1;                                
-        return 0;
+    static bool doesAliceWin(string& s) {
+        return any_of(s.begin(), s.end(), [](char c) { return ((0x208222>>(c & 0x1f)) & 1); })?1:0;
     }
 };
