6a132c3bd3554c92da2f37c178313012f242a124
Deepal Tembhurne <142749933+Deepal22@users.noreply.github.com>
2025-09-15 00:04:41 +0530

    Time: 36 ms (77.22%) | Memory: 38.8 MB (94.94%) - LeetSync

--- 1006-vowel-spellchecker/vowel-spellchecker.cpp
+++ 1006-vowel-spellchecker/vowel-spellchecker.cpp
@@ -1,47 +1,36 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
-    vector<string> spellchecker(vector<string>& wordlist, vector<string>& queries) {
-        unordered_set<string> exact(wordlist.begin(), wordlist.end());
-        unordered_map<string, string> caseMap;
-        unordered_map<string, string> vowelMap;
+    static constexpr unsigned vowMask=(1<<('A'&31))+(1<<('E'&31))+(1<<('I'&31))+(1<<('O'&31))+(1<<('U'&31));
+    static string tolow(string w) {
+        for (char& c : w) c=tolower(c);
+        return w;
+    }
 
-        for (string w : wordlist) {
-            string lower = toLower(w);
-            string devowel = deVowel(lower);
-            if (!caseMap.count(lower)) caseMap[lower] = w;
-            if (!vowelMap.count(devowel)) vowelMap[devowel] = w;
-        }
-        vector<string> result;
-        for (string q : queries) {
-            if (exact.count(q)) {
-                result.push_back(q);
-            } else {
-                string lower = toLower(q);
-                string devowel = deVowel(lower);
+    static string toDeV(string w) {
+        for (char& c : w)
+            if((vowMask>>(c&31))&1) c='$';
+        return w;
+    }
 
-                if (caseMap.count(lower)) result.push_back(caseMap[lower]);
-                else if (vowelMap.count(devowel)) result.push_back(vowelMap[devowel]);
-                else result.push_back("");
-            }
+    static vector<string> spellchecker(vector<string>& wordlist, vector<string>& queries) {
+        const int n=wordlist.size();
+        unordered_set<string> W(wordlist.begin(), wordlist.end());
+        unordered_map<string, string> Wlow, WdeV;
+        Wlow.reserve(n), WdeV.reserve(n);
+
+        for (const auto& w : wordlist) {
+            const string lw = tolow(w), dv = toDeV(lw);
+            if (!Wlow.count(lw)) Wlow[lw] = w;
+            if (!WdeV.count(dv)) WdeV[dv] = w;
         }
-        return result;
-    }
-private:
-    string toLower(string s) {
-        for (char &c : s) c = tolower(c);
-        return s;
-    }
-    string deVowel(string s) {
-        for (char &c : s) {
-            if (isVowel(c)) c = '*';
+
+        for (auto& q : queries) {
+            if (W.count(q)) continue;
+            string low = tolow(q), deV = toDeV(low);
+            if (Wlow.count(low)) q = Wlow[low];
+            else if (WdeV.count(deV)) q = WdeV[deV];
+            else q = "";
         }
-        return s;
-    }
-    bool isVowel(char c) {
-        c = tolower(c);
-        return c=='a'||c=='e'||c=='i'||c=='o'||c=='u';
+        return queries;
     }
 };
