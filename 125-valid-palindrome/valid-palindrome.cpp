2a0ee2b7128fbc4e001dfeab9aee2f68504ecab5
Deepal Tembhurne <142749933+Deepal22@users.noreply.github.com>
2025-10-30 23:58:28 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (6.12%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,17 +1,49 @@
 class Solution {
+    private:
+        bool valid(char ch){
+            if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9')){
+                return 1;
+            }
+            return 0;
+        }
+
+        char toLowercase(char ch){
+            if((ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9')){
+                return ch;
+            } else {
+                char temp = ch - 'A' + 'a';
+                return temp;
+            }
+        }
+
+        bool checkPalindrome(string a){
+            int start = 0;
+            int end = a.length() - 1;
+
+            while(start <= end){
+                if(a[start] != a[end]){
+                    return 0;
+                } else{
+                    start++;
+                    end--;
+                }
+            }
+            return 1;
+        }
 public:
     bool isPalindrome(string s) {
-       int start=0;
-       int end=s.size()-1;
-       while(start<=end){
-           if(!isalnum(s[start])){start++; continue;}
-           if(!isalnum(s[end])){end--;continue;}
-           if(tolower(s[start])!=tolower(s[end]))return false;
-           else{
-               start++;
-               end--;
-           }
+       string temp = "";
+
+       for(int j = 0; j < s.length(); j++){
+        if(valid(s[j])){
+            temp.push_back(s[j]);
+        }
        }
-       return true;
+
+       for(int j = 0; j < temp.length(); j++){
+        temp[j] = toLowercase(temp[j]);
+       }
+
+       return checkPalindrome(temp);
 }
 };
