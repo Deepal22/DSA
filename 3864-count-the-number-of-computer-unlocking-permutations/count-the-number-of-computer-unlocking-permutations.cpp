9c8f28f3be184a3f2ae66294ea6a4bd0509928aa
Deepal Tembhurne <142749933+Deepal22@users.noreply.github.com>
2025-12-10 23:33:24 +0530

    Time: 0 ms (100.00%) | Memory: 92.8 MB (71.00%) - LeetSync

--- 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.cpp
+++ 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    static int countPermutations(vector<int>& complexity) {
-        const int x0=complexity[0], 
-        x1=*min_element(complexity.begin()+1, complexity.end()), 
-        mod=1e9+7, n=complexity.size();
+    int MOD = 1e9 + 7;
+    
+    int countPermutations(vector<int>& comp) {
+        int n = comp.size();
+        long long ans = 1;
 
-        if (x0>=x1) return 0;
-        long long cnt=1;
-        for(int i=1; i<=n-1; i++){
-            cnt*=i;
-            if (cnt>=mod) cnt%=mod;
+        for (int i = 1; i < n; i++) {
+            if (comp[i] <= comp[0]) return 0;
+            ans *= i;
+            ans %= MOD;
         }
-        return cnt;
-        
+
+        return ans;
     }
 };
