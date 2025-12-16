248566da99b654ea5c87807a4aadaf7ca311717a
Deepal Tembhurne <142749933+Deepal22@users.noreply.github.com>
2025-12-16 23:30:30 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (52.44%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -1,47 +1,77 @@
 class Solution {
 public:
-    
-    long long subArrayRanges(vector<int>& nums) {
-        int n=nums.size();
-        long long sum=0;
-        stack<int>st;
-        vector<int> minPrev(n,-1),minNext(n,n),maxPrev(n,-1),maxNext(n,n);
-        
-        for(int i=0;i<n;i++)
-        {
-            while(!st.empty()&&nums[st.top()]>=nums[i]){st.pop();}
-            if(!st.empty()){minPrev[i]=st.top();}
-            st.push(i);
+    long long sumSubarrayMins(vector<int>& arr) {
+        int n = arr.size();
+        // Arrays to store distances to the previous/next smaller element
+        vector<int> lefts(n), rights(n);
+        stack<int> s;
+
+        //next smaller element
+        for(int i=n-1;i>=0;i--){
+            while(!s.empty() && arr[s.top()]>=arr[i]){
+                s.pop();
+            }
+            if (s.empty()) rights[i] =n;
+            else rights[i] =s.top();
+            s.push(i);
         }
-        while(!st.empty()){st.pop();}
-        for(int i=n-1;i>=0;i--)
-        {
-            while(!st.empty()&&nums[st.top()]>nums[i]){st.pop();}
-            if(!st.empty()){minNext[i]=st.top();}
-            st.push(i);
+        while(!s.empty()) s.pop();
+        //previous smaller and equal element
+        for(int i=0;i<n;i++){
+            while(!s.empty() && arr[s.top()]>arr[i]){
+                s.pop();
+            }
+            lefts[i]= s.empty() ? -1:s.top();
+            s.push(i);
         }
-        
-         for(int i=0;i<n;i++)
-         {
-            while(!st.empty()&&nums[st.top()]<=nums[i]){st.pop();}
-            if(!st.empty()){maxPrev[i]=st.top();}
-            st.push(i);
-         }
-         while(!st.empty()){st.pop();}
-         for(int i=n-1;i>=0;i--)
-         {
-            while(!st.empty()&&nums[st.top()]<nums[i]){st.pop();}
-            if(!st.empty()){maxNext[i]=st.top();}
-            st.push(i);
-         }
-        
-        for(int i=0;i<n;i++)
-        {
-            long long leftMin=i-minPrev[i],rightMin=minNext[i]-i;
-            long long leftMax=i-maxPrev[i],rightMax=maxNext[i]-i;
-            sum+=(leftMax*rightMax-leftMin*rightMin)*nums[i];
-            
+        long long total = 0;
+        for(int i=0;i<n;i++){
+            int l= i-lefts[i];
+            int r=rights[i]-i;
+            long long count=(long long) l*r;
+            long long contribution=count*arr[i];
+            total =total+contribution;
         }
-        return sum;
+        return total;
+    }
+    long long sumSubarrayMaxs(vector<int>& arr) {
+        int n = arr.size();
+        // Arrays to store distances to the previous/next smaller element
+        vector<int> leftl(n), rightl(n);
+        stack<int> s;
+
+        //next larger element
+        for(int i=n-1;i>=0;i--){
+            while(!s.empty() && arr[s.top()]<=arr[i]){
+                s.pop();
+            }
+            if (s.empty()) rightl[i] =n;
+            else rightl[i] =s.top();
+            s.push(i);
+        }
+        while(!s.empty()) s.pop();
+        //previous larger and equal element
+        for(int i=0;i<n;i++){
+            while(!s.empty() && arr[s.top()]<arr[i]){
+                s.pop();
+            }
+            leftl[i]= s.empty() ? -1:s.top();
+            s.push(i);
+        }
+        long long total = 0;
+        for(int i=0;i<n;i++){
+            int l= i-leftl[i];
+            int r=rightl[i]-i;
+            long long count=(long long) l*r;
+            long long contribution=count*arr[i];
+            total =total+contribution;
+        }
+        return total;
+    }
+    long long subArrayRanges(vector<int>& arr) {
+        int n=arr.size();
+        long long largest_total=sumSubarrayMaxs(arr);
+        long long smallest_total=sumSubarrayMins(arr);
+        return largest_total-smallest_total;
     }
 };
