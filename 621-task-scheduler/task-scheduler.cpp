f8259afe87449e7ef6bd53003521ceb86bdb521e
Deepal Tembhurne <142749933+Deepal22@users.noreply.github.com>
2025-12-25 23:26:56 +0530

    Time: 0 ms (100.00%) | Memory: 38.4 MB (69.11%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,38 +1,23 @@
+#include<algorithm>
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        priority_queue<int> pq;
-        vector<int>mp(26,0);
-
-        for(char i:tasks){
-            mp[i-'A']++;  // count the number of times a task needs to be done
-        }   
-        for(int i=0;i<26;++i){
-            if(mp[i]) 
-            pq.push(mp[i]);
-        }
-
-        int time=0; // stores the total time taken 
-        while(!pq.empty()){
-            vector<int>remain;
-            int cycle=n+1;  // n+1 is the CPU cycle length, if n is the cooldown period then after a task A there will be n more tasks. Hence n+1.
-
-            while(cycle and !pq.empty()){
-                int max_freq=pq.top(); // the task at the top should be first assigned the CPU as it has highest frequency
-                pq.pop();
-                if(max_freq>1){ // task with more than one occurrence, the next occurrence will be done in the next cycle 
-                    remain.push_back(max_freq-1); // add it to remaining task list
-                }
-                ++time; 
-                --cycle; 
+        int mx=0, mxCount=0;
+        vector<int> freq(26, 0);
+        for(auto c: tasks){
+            freq[c-'A']++;
+            if(mx<freq[c-'A']){
+                mx=freq[c-'A'];
+                mxCount=1;
             }
-
-            for(int count:remain){
-                pq.push(count); 
-            }
-            if(pq.empty())break; 
-            time+=cycle; 
+            else if(mx==freq[c-'A'])
+                mxCount++;
         }
-        return time;
+        int partCount = mx-1;
+        int partLength = n-(mxCount-1);
+        int emptySlots = partCount*partLength;
+        int availableTasks = tasks.size() - mx*mxCount;
+        int idles = max(0, emptySlots - availableTasks);
+        return tasks.size()+idles;
     }
 };
