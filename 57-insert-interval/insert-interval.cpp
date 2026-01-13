5a738ba6f786efa5441790837e158978b2b67bca
Deepal Tembhurne <142749933+Deepal22@users.noreply.github.com>
2026-01-13 21:53:13 +0530

    Time: 4 ms (17.02%) | Memory: 21.6 MB (93.76%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,20 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        intervals.push_back(newInterval);
-        sort(intervals.begin(), intervals.end());
+        if (intervals.empty()) {
+            return {newInterval};
+        }
 
-        vector<vector<int>> res;
-        res.push_back(intervals[0]);
+        int n = intervals.size();
+        int target = newInterval[0];
+        int left = 0, right = n - 1;
 
-        for (int i = 1; i < intervals.size(); ++i) {
-            if (res.back()[1] >= intervals[i][0]) {
-                res.back()[1] = max(res.back()[1], intervals[i][1]);
+        //binary search
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            if (intervals[mid][0] < target) {
+                left = mid + 1;
             } else {
-                res.push_back(intervals[i]);
+                right = mid - 1;
             }
         }
 
-        return res;        
+        //insert newInterval at the location
+        intervals.insert(intervals.begin() + left, newInterval);
+
+        vector<vector<int>> res;
+
+        for (const auto& interval : intervals) {
+            if (res.empty() || res.back()[1] < interval[0]) {
+                res.push_back(interval);
+            } else {
+                res.back()[1] = max(res.back()[1], interval[1]);
+            }
+        }
+
+        return res;
     }
 };
