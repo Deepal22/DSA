fdc8741304c0033c6fa7101031e2672ca9529b12
Deepal Tembhurne <142749933+Deepal22@users.noreply.github.com>
2026-01-21 21:41:40 +0530

    Time: 8 ms (24.32%) | Memory: 14.5 MB (99.76%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
@@ -9,60 +9,54 @@
  */
 class Solution {
 public:
+    vector<int> ans;
+    void distanceRoot(TreeNode* root, int k){
+        if(root == NULL || k<0){
+            return;
+        }
+        if(k == 0){
+            ans.push_back(root->val);
+            return;
+        }
+        
+        distanceRoot(root->left,k-1);
+        distanceRoot(root->right,k-1);
+    }
+    int distance(TreeNode* root, TreeNode* target, int k) {
+        if(root == NULL){
+            return -1;
+        }
+        if(target == root){
+            distanceRoot(root, k);
+            return 0;
+        }
+        
+        int dl = distance(root->left, target, k);
+        if(dl != -1){
+            if(dl+1 == k){
+                ans.push_back(root->val);
+            }
+            else{
+                distanceRoot(root->right, k-dl-2);
+            }
+            return 1+dl;
+        }
+        int dr = distance(root->right, target, k);
+        if(dr != -1){
+            if(dr+1 == k){
+                ans.push_back(root->val);
+            }
+            else{
+                distanceRoot(root->left, k-dr-2);
+            }
+            return 1+dr;
+        }
+        return -1;
+    }
+    
+
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        vector<int> ans;
-        unordered_map<int, TreeNode*> parent;
-        queue<TreeNode*> q;
-        q.push(root);
-
-        while(!q.empty()){
-            int si = q.size();
-            for(int i = 0; i < si; i++){
-                auto top = q.front();
-                q.pop();
-                
-                if(top -> left){
-                    parent[top->left->val] = top;
-                    q.push(top->left); 
-                }
-
-                if(top -> right){
-                    parent[top->right->val] = top;
-                    q.push(top->right); 
-                }
-            }
-        }
-
-        unordered_map<int, int> visited;
-        q.push(target);
-        while(k-- && !q.empty()){
-            int size = q.size();
-
-            for(int i = 0; i < size; i++){
-                auto top = q.front();
-                q.pop();
-
-                visited[top -> val] = 1;
-
-                if(top -> left && !visited[top->left->val]){
-                    q.push(top -> left);
-                }
-
-                if(top -> right && !visited[top->right->val]){
-                    q.push(top -> right);
-                }
-
-                if(parent[top->val] && !visited[parent[top->val] -> val]){
-                    q.push(parent[top->val]);
-                }
-
-            }
-        }
-
-        while(!q.empty()){
-            ans.push_back(q.front()->val);
-            q.pop();
-        }
-        return ans;
+       distance(root, target, k);
+       return ans;
     }
 };
