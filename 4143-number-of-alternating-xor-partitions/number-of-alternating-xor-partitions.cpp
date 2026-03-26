68dfbf8182188f74926e4d5234482d805ad1564b
Deepal Tembhurne <142749933+Deepal22@users.noreply.github.com>
2026-03-26 16:58:52 +0530

    Time: 3 ms (99.26%) | Memory: 138.3 MB (99.11%) - LeetSync

--- 4143-number-of-alternating-xor-partitions/number-of-alternating-xor-partitions.cpp
+++ 4143-number-of-alternating-xor-partitions/number-of-alternating-xor-partitions.cpp
@@ -1,31 +1,39 @@
 class Solution {
 public:
     int alternatingXOR(vector<int>& nums, int target1, int target2) {
-        const int MOD = 1000000007;
         int n = nums.size();
-        int prefix = 0;
-        vector<int> state = {0, target1,  target1^target2, target2};
-        vector<int> dp(4);
-        vector<int> pre_dp(4);
-        pre_dp[0] = 1;
-        dp[0] = 1;
-        for(int i=0; i<n; i++){
-            prefix ^= nums[i];
-            if(i == n-1){
-                int total = 0;
-                for(int j=0; j<4; j++){
-                    if(prefix == state[j])
-                        total =  (total + pre_dp[(j+3)%4])%MOD;
-                }
-                return total;
+        const int mod = 1e9 + 7;
+        long long ways0 = 1;
+        long long ways1 = 0;
+        long long ways2 = 0;
+        long long ways3 = 0;
+        long long ans=0;
+        int curr=0;
+        for(int i=0;i<n;i++){
+            curr ^= nums[i];
+            long long add0=0, add1=0, add2=0,add3=0;
+            //cycle is 0->T1->T1^T2->T2->0
+            if(curr==target1){
+                add1 = ways0;
             }
-            for(int j=0; j<4; j++){
-                if(prefix == state[j])
-                    dp[j] = (dp[j] + pre_dp[(j+3)%4])%MOD;
+            if(curr==( (target1)^(target2))){
+                add2 = ways1;
             }
-            pre_dp = dp;
+            if(curr == target2){
+                add3 = ways2;
+            }
+            if(curr==0){
+                add0 = ways3;
+            }
+            ways0 = (ways0+add0)%mod;
+            ways1 = (ways1+add1)%mod;
+            ways2 = (ways2+add2)%mod;
+            ways3 = (ways3+add3)%mod;
+            if(i==n-1){
+                ans = (add0+add1+add2+add3)%mod;
+            }
+            
         }
-       
-        return 0;
+        return ans;
     }
 };
