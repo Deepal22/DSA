6213b680ecb5a5e80d380e25702c3b35f6286641
Deepal Tembhurne <142749933+Deepal22@users.noreply.github.com>
2026-04-02 16:21:49 +0530

    Time: 20 ms (100.00%) | Memory: 106.5 MB (96.17%) - LeetSync

--- 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
+++ 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
     int maximumAmount(vector<vector<int>>& coins) {
-        int n = coins.size(), m = coins[0].size();
-        vector dp(n, vector(m, vector<int>(3, -1e9)));
-        dp[0][0][1] = dp[0][0][2] = 0, dp[0][0][0] = coins[0][0];
-        
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < m; j++)
-                for (int k = 0; k < 3; k++) {
-                    if (i) dp[i][j][k] = max(dp[i][j][k], dp[i - 1][j][k] + coins[i][j]);
-                    if (i && k) dp[i][j][k] = max(dp[i][j][k], dp[i - 1][j][k - 1]);
-                    if (j) dp[i][j][k] = max(dp[i][j][k], dp[i][j - 1][k] + coins[i][j]);
-                    if (j && k) dp[i][j][k] = max(dp[i][j][k], dp[i][j - 1][k - 1]);
+        vector<vector<int>> dfs(vector<vector<int>>(coins[0].size()+1,vector<int>(3,INT_MIN/2)));
+
+
+
+        for(int i =0 ;i < coins.size();i++){
+            for(int j = 0;j < coins[0].size();j++){
+                if(i == 0 && j == 0){
+                    dfs[1][0] = coins[i][j];
+                    dfs[1][1] = max(coins[i][j],0);
+                    dfs[1][2] = max(coins[i][j],0);
                 }
-        int ans = *max_element(dp[n - 1][m - 1].begin(), dp[n - 1][m - 1].end());
-        return ans;
+                else{
+  
+                    dfs[j+1][2] = max(coins[i][j] + max(dfs[j+1][2],dfs[j][2]),max(dfs[j+1][1],dfs[j][1]));
+                    dfs[j+1][1] = max(coins[i][j] + max(dfs[j+1][1],dfs[j][1]),max(dfs[j+1][0],dfs[j][0]));
+                    dfs[j+1][0] = coins[i][j]+max(dfs[j+1][0],dfs[j][0]);
+                    
+                }
+            }
+        }
+        return max({dfs[coins[0].size()][0],dfs[coins[0].size()][1],dfs[coins[0].size()][2]});
+
+
     }
 };
